1c62929073728ee1c47c32b49b48083ff8060aad
SOLE LOLO ADAN <loloski17@gmail.com>
2024-03-27 11:39:01 +0300

    updated the home page more

--- home.php
+++ home.php
@@ -9,6 +9,10 @@
   <!--font awesome cdn link-->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
+  <!--swiper css link-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+/>
+
   <!--custom css file link-->
   <link rel="stylesheet" href="css/style.css">
 </head>
@@ -16,6 +20,42 @@
   <div class="container">
 
   <?php @include 'header.php';?>
+
+  <section class="home">
+    
+   <div class="swiper home-slider">
+
+    <div class="swiper-wrapper">
+
+     <div class="swiper-slide slide" style="background:url(images/home-slide1.jpg) no-repeat">
+     <div class="content">
+      <h3>plan your wedding</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat corrupti accusantium quisquam vero ipsa? Modi iusto sunt voluptates accusamus fuga.</p>
+      <a href="about.php" class="btn">discover more</a>
+     </div>
+
+     <div class="swiper-slide slide" style="background:url(images/home-slide2.jpg) no-repeat">
+     <div class="content">
+      <h3>plan your wedding</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat corrupti accusantium quisquam vero ipsa? Modi iusto sunt voluptates accusamus fuga.</p>
+      <a href="about.php" class="btn">discover more</a>
+     </div>
+
+     <div class="swiper-slide slide" style="background:url(images/home-slide3.jpg) no-repeat">
+     <div class="content">
+      <h3>plan your wedding</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat corrupti accusantium quisquam vero ipsa? Modi iusto sunt voluptates accusamus fuga.</p>
+      <a href="about.php" class="btn">discover more</a>
+     </div>
+
+     </div>
+
+    </div>
+
+    <div class="swiper-pagination"></div>
+
+   </div>
+  </section>
 
 
   <?php @include 'footer.php';?>
@@ -32,7 +72,8 @@
 
 
 
-
+<!--swiper js link-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
   <!--custom js file link-->
   <script src="js/script.js"></script>
